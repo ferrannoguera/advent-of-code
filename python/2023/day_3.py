96a117e5a94990bbf24cfccf474d1da93bbed437
ferrannoguera <nogueravallferran@gmail.com>
2023-12-03 18:16:40 +0100

    :zap: Improved performance of 03-2023

--- python/2023/day_3.py
+++ python/2023/day_3.py
@@ -3,7 +3,7 @@ from functools import reduce
 import numpy as np
 
 
-USER_SESSION = 'my_session'
+USER_SESSION = '53616c7465645f5f482bd5a563018311b6a153ca12437a8c490e82963acb708f58baf18173026ce6eb22b615d9957eae2f2589c99a75c636910f9637fd85aa53'
 
 
 MAP_ADJACENT_SYMBOLS = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
@@ -20,32 +20,7 @@ SAMPLE_INPUT = """467..114..
 .664.598.."""
 
 
-def _has_adjacent_symbol_2(_first_pos, _data, _found_num, _gear_nums):
-    _proc_row, _f_y = _first_pos
-    range_pos = [(_proc_row, c) for c in [c for c in range(_f_y, _f_y + len(_found_num))]]
-    check_pos = list(
-        map(lambda x: [
-            (
-                x[0] + x_adj, x[1] + y_adj
-            )
-            for x_adj, y_adj in MAP_ADJACENT_SYMBOLS
-            if (
-                    0 <= x[0] + x_adj < _data.shape[0] and 0 <= x[1] + y_adj < _data.shape[1] and
-                    (x[0] + x_adj, x[1] + y_adj) not in range_pos
-            )
-        ], range_pos)
-    )
-    flattened_list = list(set(item for sublist in check_pos for item in sublist))
-    for pos in flattened_list:
-        if _data[pos[0]][pos[1]] == '*':
-            if f'{pos[0]},{pos[1]}' in _gear_nums:
-                _gear_nums[f'{pos[0]},{pos[1]}'].append(int(_found_num))
-            else:
-                _gear_nums[f'{pos[0]},{pos[1]}'] = [int(_found_num)]
-    return _gear_nums
-
-
-def _has_adjacent_symbol(_first_pos, _data, _found_num):
+def _parse_schematic(_first_pos, _data, _found_num, _gear_nums):
     _proc_row, _f_y = _first_pos
     range_pos = [(_proc_row, c) for c in [c for c in range(_f_y, _f_y + len(_found_num))]]
     check_pos = list(
@@ -60,11 +35,19 @@ def _has_adjacent_symbol(_first_pos, _data, _found_num):
             )
         ], range_pos)
     )
+    return_value = False
     flattened_list = list(set(item for sublist in check_pos for item in sublist))
     for pos in flattened_list:
         if _data[pos[0]][pos[1]] != '.' and not _data[pos[0]][pos[1]].isdigit():
-            return int(_found_num)
-    return 0
+            return_value = True
+        if _data[pos[0]][pos[1]] == '*':
+            if f'{pos[0]},{pos[1]}' in _gear_nums:
+                _gear_nums[f'{pos[0]},{pos[1]}'].append(int(_found_num))
+            else:
+                _gear_nums[f'{pos[0]},{pos[1]}'] = [int(_found_num)]
+    if return_value:
+        return int(_found_num), _gear_nums
+    return 0, _gear_nums
 
 
 if __name__ == '__main__':
@@ -76,8 +59,8 @@ if __name__ == '__main__':
     first_pos = None
     for row in range(parsed_input_data.shape[0]):
         if found_num:
-            sum_valid_numbers += _has_adjacent_symbol(first_pos, parsed_input_data, found_num)
-            gear_nums = _has_adjacent_symbol_2(first_pos, parsed_input_data, found_num, gear_nums)
+            valid_num, gear_nums = _parse_schematic(first_pos, parsed_input_data, found_num, gear_nums)
+            sum_valid_numbers += valid_num
         found_num = None
         first_pos = None
         for col in range(parsed_input_data.shape[1]):
@@ -87,8 +70,8 @@ if __name__ == '__main__':
             elif found_num and parsed_input_data[row][col].isdigit():
                 found_num += parsed_input_data[row][col]
             elif found_num and not parsed_input_data[row][col].isdigit():
-                sum_valid_numbers += _has_adjacent_symbol(first_pos, parsed_input_data, found_num)
-                gear_nums = _has_adjacent_symbol_2(first_pos, parsed_input_data, found_num, gear_nums)
+                valid_num, gear_nums = _parse_schematic(first_pos, parsed_input_data, found_num, gear_nums)
+                sum_valid_numbers += valid_num
                 found_num = None
                 first_pos = None
     print(f'Part 1: {sum_valid_numbers}')
